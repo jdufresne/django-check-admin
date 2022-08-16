65d1fd62051ab6003da9036de52ecf99942aaa23
François Freitag <mail@franek.fr>
2022-08-16 12:02:39 +0200

    Update to support newer Django & Python, drop old versions
    Python 3.6 has reached its end of life.
    Django version under 3.2 are no longer supported.

--- checkadmin/__init__.py
+++ checkadmin/__init__.py
@@ -1,6 +1,5 @@
 from typing import Set
 
-import django
 from django.apps import apps as django_apps
 from django.conf import settings
 from django.db import models
@@ -18,7 +17,3 @@ def get_ignored() -> Set[models.Model]:
         model = django_apps.get_model(*model_path.rsplit(".", 1), False)
         ignored.add(model)
     return ignored
-
-
-if django.VERSION < (3, 2):
-    default_app_config = "checkadmin.apps.CheckAdminConfig"
